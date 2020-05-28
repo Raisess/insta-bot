0f09ed83f9da6399514754d8cf566648cca1d726
danillu <48914278+Raisess@users.noreply.github.com>
2020-05-28 10:55:17 -0300

    created login module

--- src/modules/login.js
+++ src/modules/login.js
@@ -25,5 +25,36 @@ if (require('../credencials.json')) {
 module.exports = async page => {
   const navigationPromise = page.waitForNavigation();
 
+  // ir para url de login
   await page.goto(login_url);
+
+  await navigationPromise;
+
+  // clicar no campo de usuario
+  await page.waitForSelector('input[name="username"]');
+  await page.click('input[name="username"]');
+
+  await navigationPromise;
+
+  // escrever nome de usuario
+  await page.type('input[name="username"]', yourUser);
+
+  // clicar no campo de senha
+  await page.waitForSelector('input[name="password"]');
+  await page.click('input[name="password"]');
+
+  await navigationPromise;
+
+  // escrever a senha
+  await page.type('input[name="password"]', yourPass);
+
+  // clicar em "Entrar"
+  await page.waitForSelector('button[class="sqdOP  L3NKy   y3zKF     "]');
+  await page.click('button[class="sqdOP  L3NKy   y3zKF     "]');
+
+  await navigationPromise;
+
+  // fechar popup de notificação
+  await page.waitForSelector('button[class="aOOlW   HoLwm "]');
+  await page.click('button[class="aOOlW   HoLwm "]');
 }
