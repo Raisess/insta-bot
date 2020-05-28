fe91765aaf2a5d8d9faaaa140132c69e17a01b74
danillu <48914278+Raisess@users.noreply.github.com>
2020-05-28 11:58:30 -0300

    created comment module

--- src/modules/comment.js
+++ src/modules/comment.js
@@ -1,5 +1,5 @@
 module.exports = async page => {
-  const navigationPromise = page.waitForNavigation();
+  // const navigationPromise = page.waitForNavigation();
 
   // clicar no campo de comentario
   await page.waitForSelector('span[class="_15y0l"]');
@@ -7,5 +7,9 @@ module.exports = async page => {
 
   // escrever comentario
   console.log('escrevendo...');
-  await page.keyboard.type('teste');
+  await page.keyboard.type('@reidner03');
+
+  // clicar em publicar
+  await page.waitForSelector('button[class="sqdOP yWX7d    y3zKF     "]');
+  await page.click('button[class="sqdOP yWX7d    y3zKF     "]');
 }
